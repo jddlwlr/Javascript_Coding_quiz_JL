cd3387716e7d79731d52238daa6bd871dcf17dc0
Jesse Lawler <jedd.lawler@gmail.com>
2021-11-20 20:06:23 -0700

    Adding functionality, updated js and html

--- assets/script.js
+++ assets/script.js
@@ -62,27 +62,3 @@ function nextQuestion () {
 // need to have the function inside the event listenr
 
 $choices.click(compare()) 
-
-// if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-//   localStorage.setItem("mostRecentScore", score);
-
-//   return window.location.assign("./end.html");
-// }
-
-// questionCounter++;
-// progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
-// progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
-
-// const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
-// currentQuestion = availableQuestions[questionsIndex];
-// question.innerText = currentQuestion.question;
-
-// choices.forEach((choice) => {
-//   const number = choice.dataset["number"];
-//   choice.innerText = currentQuestion["choice" + number];
-// });
-
-// availableQuestions.splice(questionsIndex, 1);
-
-// acceptingAnswers = true;
-// };
